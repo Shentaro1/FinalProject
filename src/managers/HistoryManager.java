da2afd9fce57324d891f170984040b398798560d
Ivan <svaild793@gmail.com>
2025-07-13 14:26:08 +0300

    version2

--- src/managers/HistoryManager.java
+++ src/managers/HistoryManager.java
@@ -1,4 +1,10 @@
 package managers;
 
+import tasks.AbstractTask;
+import java.util.ArrayList;
+
 public interface HistoryManager {
+    void add(AbstractTask task);
+    ArrayList<AbstractTask> getHistory();
+    void remove(int id);
 }
--- src/tasks/AbstractTask.java
+++ src/tasks/AbstractTask.java
@@ -1,4 +1,72 @@
 package tasks;
+import types.Status;
+import java.util.Objects;
 
-public class AbstractTask {
+public abstract class AbstractTask {
+    private int id;
+    private String name;
+    private String description;
+    protected Status status;
+
+    AbstractTask(AbstractTask abstractTask) {
+        this(abstractTask.id, abstractTask.description, abstractTask.name, abstractTask.status);
+    }
+
+    AbstractTask(int id, String description, String name, Status status) {
+        this.description = description;
+        this.name = name;
+        this.id = id;
+        this.status = status;
+    }
+
+    AbstractTask(String description, String name) {
+        this(0, description, name, Status.NEW);
+    }
+
+    //set
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //get
+    public String getDescription() {
+        return description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public
+
+    @Override
+    boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof AbstractTask)) return false;
+        AbstractTask that = (AbstractTask) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public abstract AbstractTask copy();
 }
