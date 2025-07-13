a7b9924cf49bfc4429eb9549b152c94cfa394779
Ivan <svaild793@gmail.com>
2025-07-13 15:11:14 +0300

    version8(create tests)

--- src/managers/HistoryManager.java
+++ src/managers/HistoryManager.java
@@ -4,7 +4,11 @@ import tasks.AbstractTask;
 import java.util.ArrayList;
 
 public interface HistoryManager {
+
     void add(AbstractTask task);
+
     ArrayList<AbstractTask> getHistory();
+
     void remove(int id);
+
 }
