539b73b8f4540442c2126481699a158dc7651493
Ivan <svaild793@gmail.com>
2025-07-13 14:49:24 +0300

    version4(create tests)

--- src/tasks/SubTask.java
+++ src/tasks/SubTask.java
@@ -2,26 +2,26 @@ package tasks;
 import types.Status;
 
 public class SubTask extends AbstractTask {
-    EpicTask fk_epicTask;
+    EpicTask fkEpicTask;
 
     public SubTask(SubTask subTask) {
         super(subTask);
-        fk_epicTask = subTask.getFk_epicTask();
+        fkEpicTask = subTask.getFk_epicTask();
     }
 
     public SubTask(String description, String name, EpicTask fk_epicTask) {
         super(description, name);
-        this.fk_epicTask = fk_epicTask;
+        this.fkEpicTask = fk_epicTask;
     }
 
     //get
     public EpicTask getFk_epicTask() {
-        return fk_epicTask;
+        return fkEpicTask;
     }
 
     //set
     public void setFk_epicTask(EpicTask fk_epicTask) {
-        this.fk_epicTask = fk_epicTask;
+        this.fkEpicTask = fk_epicTask;
     }
 
     public void setStatus(Status status) {
@@ -31,7 +31,7 @@ public class SubTask extends AbstractTask {
     @Override
     public String toString() {
         return "SubTask{" +
-                "fk_epicTask=" + fk_epicTask.getId() +
+                "fk_epicTask=" + fkEpicTask.getId() +
                 ", description='" + getDescription() + '\'' +
                 ", id=" + getId() +
                 ", name='" + getName() + '\'' +
