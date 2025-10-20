d169beab7951716f3c4279cf39a7e677159216e8
Ivan <svaild793@gmail.com>
2025-10-20 11:52:24 +0300

    version8(final v6)

--- src/utils/FileBackedMain.java
+++ src/utils/FileBackedMain.java
@@ -17,7 +17,7 @@ public class FileBackedMain {
 
         tm.createTask(new Task("a", "b", Status.NEW, 28, Duration.ofNanos(1), test));
         tm.createEpicTask(new EpicTask("fghjk", "fghjkloi8uytgbhnj"));
-        tm.createSubTask(new SubTask("a", "b", Status.NEW, 10, tm.getAllEpicTask().getFirst() ,Duration.ofNanos(1), LocalDateTime.of(2019, 03, 28, 14, 33, 48, 640000)));
+        tm.createSubTask(new SubTask("a", "b", Status.NEW, 10, tm.getAllEpicTask().getFirst() ,Duration.ofNanos(1), LocalDateTime.of(2019, 3, 28, 14, 33, 48, 640000)));
         tm.createTask(new Task("ertyuiop", " r567890okijm", Status.NEW, 48, Duration.ofNanos(1), LocalDateTime.now()));
 //        Task task = tm.getAllTask().get(1);
 //        task.setStartTime(LocalDateTime.now());
